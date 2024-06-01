2c6cfc5b9ba73a057dc2b27a67ddd9872b10274f
jhrdt <jhrdt@noreply.localhost>
2024-06-01 09:19:27 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+import machine
+import uos
+from machine import I2C, Pin
+
+# Third-Party
+import sdcard
+from eeprom import EEPROM
+from epaper import EPD_5in65
+
+# Local
+import bitmap
+
+
+# Fns
+#
+def display_bytes(epd, bytes_gen):
+    epd.send_command(0x61)   # Set Resolution setting
+    epd.send_data(0x02)
+    epd.send_data(0x58)
+    epd.send_data(0x01)
+    epd.send_data(0xC0)
+    epd.send_command(0x10)
+
+    for b in bytes_gen:
+        epd.digital_write(epd.dc_pin, 1)
+        epd.digital_write(epd.cs_pin, 0)
+        epd.spi.write(b)
+        epd.digital_write(epd.cs_pin, 1)
+
+    epd.send_command(0x04)   # 0x04
+    epd.BusyHigh()
+    epd.send_command(0x12)   # 0x12
+    epd.BusyHigh()
+    epd.send_command(0x02)   # 0x02
+    epd.BusyLow()
+    epd.delay_ms(200)
+
+def clean_display(epd):
+    epd.EPD_5IN65F_Clear(epd.White)
+    epd.EPD_5IN65F_Clear(epd.Clear)
+
+def poweroff(i):
+    pin = Pin(i, Pin.OUT)
+    pin.high()
+    #time.sleep(5)
+    pin.low()
+
+
+# Constants
+#
+IMAGE_DIR = "/imgs"
+DONE_PIN = 16
+
+
+# TODO
+# If switch if off
+# poweroff(DONE_PIN)
+
+
+# EEPROM
+#
+I2C_ADDR = 0x50     # DEC 80, HEX 0x50
+EEPROM_SIZE = 16    # AT24C16 on 0x50
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400_000)
+eeprom = EEPROM(addr=I2C_ADDR, at24x=EEPROM_SIZE, i2c=i2c)
+
+rom_data = eeprom.read(1, 3)
+try:
+    image_i = int(rom_data)
+except ValueError:
+    image_i = 0
+
+
+# SD Card Reader
+#
+spi = machine.SPI(
+    0,
+    baudrate=1_000_000,
+    polarity=0,
+    phase=0,
+    bits=8,
+    firstbit=machine.SPI.MSB,
+    sck=machine.Pin(2),
+    mosi=machine.Pin(3),  # TX
+    miso=machine.Pin(4)  # RX
+)
+cs = machine.Pin(5, machine.Pin.OUT)
+sd = sdcard.SDCard(spi, cs)
+
+vfs = uos.VfsFat(sd)
+uos.mount(vfs, IMAGE_DIR)
+
+images = [item for item in sorted(uos.listdir(IMAGE_DIR)) if item.endswith(".bmp")]
+
+if image_i >= len(images):
+    image_i = 0
+
+# TODO
+# image_i += 1
+eeprom.write(1, f"{image_i:03d}")  # Write next position
+
+bmp_image = images[image_i]
+
+
+# ePaper Display
+#
+epd = EPD_5in65()
+epd.EPD_5IN65F_Clear(epd.White)
+with open(f"{IMAGE_DIR}/{bmp_image}", "rb") as img:
+    bytestream = bitmap.as_bytes(img)
+    display_bytes(epd, bytestream)
+
+# TODO
+# Shutdown
+#
+#poweroff(DONE_PIN)
