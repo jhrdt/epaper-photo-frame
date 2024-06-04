b58fe3c5719ec2a654ec7f33f23b2cef978ff06f
jhrdt <jhrdt@noreply.localhost>
2024-06-04 19:22:38 +0000

    Update lib/bitmap.py

--- lib/bitmap.py
+++ lib/bitmap.py
@@ -1,25 +1,25 @@
 import struct
 
-def as_shorts(i):
-    """Reads a integer as two 4bit shorts, represented as two ints."""
-    if isinstance(i, bytes):
-        i = ord(i)
-    four_bit_mask = 0b00001111
-    a = i & four_bit_mask  # leading 4 bits
-    b = i >> 4  # trailing 4 bits
-    return (b, a)
+# def as_shorts(i):
+#     """Reads a integer as two 4bit shorts, represented as two ints."""
+#     if isinstance(i, bytes):
+#         i = ord(i)
+#     four_bit_mask = 0b00001111
+#     a = i & four_bit_mask  # leading 4 bits
+#     b = i >> 4  # trailing 4 bits
+#     return (b, a)
 
 def as_int(tuple):
     return tuple[0]
 
 def bmp_header(fh):
-    type = fh.read(2).decode()
+    type_ = fh.read(2).decode()
     size =  struct.unpack('I', fh.read(4))
     reserved_1 = struct.unpack('H', fh.read(2))
     reserved_2 = struct.unpack('H', fh.read(2))
     offset = struct.unpack('I', fh.read(4))
     result = {
-        "type": type,
+        "type": type_,
         "size": size,
         "reserved_1": reserved_1,
         "reserved_2": reserved_2,
@@ -58,38 +58,38 @@ def bmp_dib_header(fh):
     return result
 
 
-def pixel_as_bytes(fh):
-    fh.seek(0)
-    header = bmp_header(fh)
-    dib_header = bmp_dib_header(fh)
+# def pixel_as_bytes(fh):
+#     fh.seek(0)
+#     header = bmp_header(fh)
+#     dib_header = bmp_dib_header(fh)
 
-    offset = header["offset"]
-    fh.seek(offset)
+#     offset = header["offset"]
+#     fh.seek(offset)
 
-    width = dib_header["width"]
-    height = dib_header["height"]
-    assert width * height == 600 * 448, "Expected size 600x448"
+#     width = dib_header["width"]
+#     height = dib_header["height"]
+#     assert width * height == 600 * 448, "Expected size 600x448"
 
-    bit_per_pixel = dib_header["bit_per_pixel"]
-    assert bit_per_pixel == 4, "Expected 4 bits per pixel"
+#     bit_per_pixel = dib_header["bit_per_pixel"]
+#     assert bit_per_pixel == 4, "Expected 4 bits per pixel"
 
-    num_of_colours = dib_header["number_of_colours"]
-    assert num_of_colours == 7, "Expected 7 colours"
+#     num_of_colours = dib_header["number_of_colours"]
+#     assert num_of_colours == 7, "Expected 7 colours"
 
-    bits_per_width = bit_per_pixel * width
-    bytes_per_width = int(bits_per_width / 8)
+#     bits_per_width = bit_per_pixel * width
+#     bytes_per_width = int(bits_per_width / 8)
 
-    pixels = width * height
-    buf = [0x00] * pixels
-    for line_no in range(height):
-        row = height - (line_no + 1)
-        offset = row * width
-        for i in range(bytes_per_width):
-            single_byte = fh.read(1)  # two pixels for 1 byte
-            short1, short2 = as_shorts(single_byte)
-            buf[offset + (i * 2)] = short1
-            buf[offset + (i * 2) + 1] = short2
-    return bytes(buf)
+#     pixels = width * height
+#     buf = [0x00] * pixels
+#     for line_no in range(height):
+#         row = height - (line_no + 1)
+#         offset = row * width
+#         for i in range(bytes_per_width):
+#             single_byte = fh.read(1)  # two pixels for 1 byte
+#             short1, short2 = as_shorts(single_byte)
+#             buf[offset + (i * 2)] = short1
+#             buf[offset + (i * 2) + 1] = short2
+#     return bytes(buf)
 
 def as_bytes(fh):
     fh.seek(0)
@@ -114,8 +114,8 @@ def as_bytes(fh):
             byte = fh.read(1)
             yield byte
 
-def as_ints(fh):
-    for b in as_bytes(fh):
-        short1, short2 = as_shorts(b)
-        yield short1
-        yield short2
+# def as_ints(fh):
+#     for b in as_bytes(fh):
+#         short1, short2 = as_shorts(b)
+#         yield short1
+#         yield short2
