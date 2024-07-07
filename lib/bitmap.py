2d396c5e228cb2390dc29b03af34cad90cc8ef9b
jhrdt <jhrdt@noreply.localhost>
2024-07-07 18:42:42 +0000

    Update lib/bitmap.py

--- lib/bitmap.py
+++ lib/bitmap.py
@@ -1,16 +1,8 @@
 import struct
 
-# def as_shorts(i):
-#     """Reads a integer as two 4bit shorts, represented as two ints."""
-#     if isinstance(i, bytes):
-#         i = ord(i)
-#     four_bit_mask = 0b00001111
-#     a = i & four_bit_mask  # leading 4 bits
-#     b = i >> 4  # trailing 4 bits
-#     return (b, a)
-
 def as_int(tuple):
     return tuple[0]
+
 
 def bmp_header(fh):
     type_ = fh.read(2).decode()
@@ -27,6 +19,7 @@ def bmp_header(fh):
     }
     fh.seek(0)
     return result
+
 
 def bmp_dib_header(fh):
     fh.seek(14)
@@ -58,39 +51,6 @@ def bmp_dib_header(fh):
     return result
 
 
-# def pixel_as_bytes(fh):
-#     fh.seek(0)
-#     header = bmp_header(fh)
-#     dib_header = bmp_dib_header(fh)
-
-#     offset = header["offset"]
-#     fh.seek(offset)
-
-#     width = dib_header["width"]
-#     height = dib_header["height"]
-#     assert width * height == 600 * 448, "Expected size 600x448"
-
-#     bit_per_pixel = dib_header["bit_per_pixel"]
-#     assert bit_per_pixel == 4, "Expected 4 bits per pixel"
-
-#     num_of_colours = dib_header["number_of_colours"]
-#     assert num_of_colours == 7, "Expected 7 colours"
-
-#     bits_per_width = bit_per_pixel * width
-#     bytes_per_width = int(bits_per_width / 8)
-
-#     pixels = width * height
-#     buf = [0x00] * pixels
-#     for line_no in range(height):
-#         row = height - (line_no + 1)
-#         offset = row * width
-#         for i in range(bytes_per_width):
-#             single_byte = fh.read(1)  # two pixels for 1 byte
-#             short1, short2 = as_shorts(single_byte)
-#             buf[offset + (i * 2)] = short1
-#             buf[offset + (i * 2) + 1] = short2
-#     return bytes(buf)
-
 def as_bytes(fh):
     fh.seek(0)
     header = bmp_header(fh)
@@ -113,9 +73,3 @@ def as_bytes(fh):
         for i in range(bytes_per_width):
             byte = fh.read(1)
             yield byte
-
-# def as_ints(fh):
-#     for b in as_bytes(fh):
-#         short1, short2 = as_shorts(b)
-#         yield short1
-#         yield short2
